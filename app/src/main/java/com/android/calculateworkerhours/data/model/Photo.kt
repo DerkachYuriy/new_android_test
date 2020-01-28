c89f4c3e4f10825d137ac901e65dae8f769d890a
Derkach Yuriy <yuriy.d@attractgroup.com>
2020-01-29 00:40:37 +0200

    Koin and Coroutines dependencies added

--- app/src/main/java/com/android/calculateworkerhours/data/model/Photo.kt
+++ app/src/main/java/com/android/calculateworkerhours/data/model/Photo.kt
@@ -1,14 +1,12 @@
 package com.android.calculateworkerhours.data.model
 
 import com.google.gson.annotations.SerializedName
-import io.realm.RealmObject
-import io.realm.annotations.PrimaryKey
 import java.io.Serializable
 
-open class Photo(@PrimaryKey
+open class Photo(
                  @SerializedName("id")
                  var id: String? = null,
                  @SerializedName("small")
                  var small: String? = null,
                  @SerializedName("medium")
-                 var medium: String? = null) : RealmObject(), Serializable
+                 var medium: String? = null) :Serializable
